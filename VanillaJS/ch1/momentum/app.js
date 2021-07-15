6d3d01768d7a5a1ccb00ad4c6c69a42ccd52c704
CaChiJ <53603478+CaChiJ@users.noreply.github.com>
2021-07-15 16:07:52 +0900

    vanillajs initialie

---
+++ VanillaJS/ch1/momentum/app.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 2;
+const myName = "hj";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "hyunjun";
+
+console.log("your new name is " + myName);
