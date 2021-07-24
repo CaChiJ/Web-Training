09e3df14e3b8147f3dd68cc63f779f842c0ac524
CaChiJ <53603478+CaChiJ@users.noreply.github.com>
2021-07-24 20:59:07 +0900

    VanillaJS chapter7 - to do list

---
+++ VanillaJS/ch7/js/toDo.js
@@ -0,0 +1,50 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+let toDos = [];
+
+const TODOS_KEY = "toDos";
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+    toDos = toDos.filter((todo) => todo.id !== parseInt(li.id));
+    saveToDos();
+}
+
+function paintToDo(newToDo) {
+    const li = document.createElement("li");
+    li.id = newToDo.id;
+    const span = document.createElement("span");
+    span.innerText = newToDo.text;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newToDo = {text: toDoInput.value, id: Date.now()}
+    toDoInput.value = "";
+    toDos.push(newToDo);
+    saveToDos();
+    paintToDo(newToDo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
